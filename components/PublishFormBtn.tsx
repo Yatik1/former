330ff1da8d8a3a6b652a3bee4987f2542b25ca49
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-19 16:42:02 +0530

    Publish btn

--- components/PublishFormBtn.tsx
+++ components/PublishFormBtn.tsx
@@ -4,7 +4,7 @@ import { GitFork } from 'lucide-react'
 
 function PublishFormBtn() {
   return (
-    <Button variant={"outline"} className='gap-2 bg-white text-black'>
+    <Button variant={"outline"} className='gap-2 bg-black text-white dark:bg-white dark:text-black'>
        <GitFork className='h-4 w-4'/>
        Publish
     </Button>
