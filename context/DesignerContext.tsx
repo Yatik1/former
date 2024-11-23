3191b4b76b7a492d662ab26a83b71f35a0dacedc
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-23 23:33:28 +0530

    :: Designer context interface

--- context/DesignerContext.tsx
+++ context/DesignerContext.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { FormElementInstance } from "@/components/FormElement";
-import { createContext, useState } from "react";
+import { createContext, Dispatch, SetStateAction, useState } from "react";
 
 type DesignContextProps = {
     elements:FormElementInstance[]
     addElement:(index:number , element:FormElementInstance) => void
     removeElement:(id:string) => void
+
+    selectedElement:FormElementInstance|null
+    setSelectedElement: Dispatch<SetStateAction<FormElementInstance | null>>
 }
 
 export const DesignerContext = createContext<DesignContextProps | null>(null)
@@ -15,6 +18,8 @@ export default function DesignerContextProvider({children} : {children : React.R
 
 
     const [elements,setElements] = useState<FormElementInstance[]>([])
+    const [selectedElement,setSelectedElement] = useState<FormElementInstance | null>(null)
+
     const addElement = (index:number , element:FormElementInstance) => {
         setElements((prev) => {
             const newElement = [...prev]
@@ -32,7 +37,9 @@ export default function DesignerContextProvider({children} : {children : React.R
             value={{
                 elements,
                 addElement,
-                removeElement
+                removeElement,
+                selectedElement,
+                setSelectedElement
             }}
         >
             {children}
