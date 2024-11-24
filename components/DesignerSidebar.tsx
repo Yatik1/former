a76df165bbcf2d3ef29028eeba80b969b5578cf9
Yatik1 <yatiksrivastava1@gmail.com>
2024-11-24 23:49:23 +0530

    Designer Sidebar

--- components/DesignerSidebar.tsx
+++ components/DesignerSidebar.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { FormElements } from './FormElement'
 import SideBarElement from './SideBarElement'
+import useDesigner from '@/hooks/useDesigner'
+import FormElementsSidebar from './FormElementsSidebar'
+import PropertiesFormSidebar from './PropertiesFormSidebar'
 
 function DesignerSidebar() {
+  const {selectedElement} = useDesigner()
+
   return (
     <div className="w-[400px] max-w-[400px] flex flex-col flex-grow gap-2 border-l-2 border-muted p-4 bg-background overflow-y-auto h-full">
-        Elements
-        <SideBarElement formElement={FormElements.TextField}/>
+      {!selectedElement && <FormElementsSidebar />}
+      {selectedElement && <PropertiesFormSidebar />}
     </div>
   )
 }
