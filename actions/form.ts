648ba9071dda60f6aa5c852d207b15e27a0c515b
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-02 13:30:34 +0530

    get submissions form action

--- actions/form.ts
+++ actions/form.ts
@@ -181,3 +181,20 @@ export async function SubmitForm(formUrl:string, content:string) {
         }
     })
 }
+
+export async function GetFormWithSubmissions(id:number) {
+    const user = await currentUser()
+    if(!user) {
+        redirect("/sign-in")
+    }
+
+    return await prisma.form.findUnique({
+        where:{
+            userId:user.id,
+            id,
+        },
+        include:{
+            FormSubmissions:true
+        }
+    })
+}
