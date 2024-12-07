0d5c179d69dccd1367d587b079ed31b0cc309882
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-07 08:42:40 +0530

    Form Element

--- components/FormElement.tsx
+++ components/FormElement.tsx
@@ -1,6 +1,26 @@
+import { CheckboxFieldFormElement } from "./fields/CheckboxField";
+import { DateFieldFormElement } from "./fields/DateField";
+import { NumberFieldFormElement } from "./fields/NumberField";
+import { ParagraphFieldFormElement } from "./fields/ParagraphField";
+import { SelectFieldFormElement } from "./fields/SelectField";
+import { SeparatorFieldFormElement } from "./fields/SeparatorField";
+import { SpacerFieldFormElement } from "./fields/SpacerField";
+import { SubTitleFieldFormElement } from "./fields/SubTitleField";
+import { TextAreaFieldFormElement } from "./fields/TextAreaField";
 import { TextFieldFormElement } from "./fields/TextField";
+import { TitleFieldFormElement } from "./fields/TitleField";
 
-export type ElementType = "TextField";
+export type ElementType = "TextField" |
+                          "TitleField" |  
+                          "SubTitleField" |
+                          "ParagraphField" |
+                          "SeparatorField" | 
+                          "SpacerField" |
+                          "NumberField"|
+                          "TextAreaField" |
+                          "DateField" |
+                          "SelectField" |
+                          "CheckboxField" ;
 
 export type SubmitFunction = (key:string, value:string) => void
 
@@ -41,5 +61,15 @@ type FormElementType = {
 }
 
 export const FormElements: FormElementType = {
-    TextField : TextFieldFormElement
+    TextField : TextFieldFormElement,
+    TitleField: TitleFieldFormElement,
+    SubTitleField: SubTitleFieldFormElement,
+    ParagraphField: ParagraphFieldFormElement,
+    SeparatorField: SeparatorFieldFormElement,
+    SpacerField: SpacerFieldFormElement,
+    NumberField: NumberFieldFormElement,
+    TextAreaField: TextAreaFieldFormElement,
+    DateField: DateFieldFormElement,
+    SelectField: SelectFieldFormElement,
+    CheckboxField:CheckboxFieldFormElement
 }
