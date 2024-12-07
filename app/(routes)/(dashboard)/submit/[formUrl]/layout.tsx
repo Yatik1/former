6be21d3d1668b8c3794a8865430de2cf593f46a8
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-07 08:42:15 +0530

    layout

--- app/(routes)/(dashboard)/submit/[formUrl]/layout.tsx
+++ app/(routes)/(dashboard)/submit/[formUrl]/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function layout({children} : {children:React.ReactNode}) {
   return (
-    <div className='flex w-full flex-col flex-grow mx-auto'>
+    <div className='flex w-full h-screen flex-grow'>
         {children}
     </div>
   )
